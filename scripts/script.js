7e6411e735efa7cb8cad6a624c98a198e90dc6e1
webartistdesign-jpg <webartistdesign@gmail.com>
2025-10-07 17:48:50 -0700

    Completed instructions

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let c=a+b;
+let name ="Web Artist Design";
+
+let firstName = 'Oscar';
+let lastName = 'Soto';
+
+// Camell Casting
+// variiablesNamesStartWithLowerCaseThenUpperCaseEachNewWord
+console.log(c);
+console.log(lastName);
+console.log("My full name is" + firstName+lastName);
+//Track your variables to si if they work
+let x="2"; //string
+let y="1"; //number
+let eligibility ="true"; //boolean
+let z=x+y;//21
+
+//Variable Naming -
+let name1; let _name; let $name;
+
+//CONSTANTS GO ON UPPERCASE
+const PI=3.14;
+const BIRTHDAY='01-01-1970';
+const AGE=50;
+let radius = 2;
+let area = PI * (radius**2);
+let modulo = 14 % PI;
